386ae7f168e5fe31bd40f37dca8a5d55b4e6c70d
lbuse <lucasbusellato@gmail.com>
2022-04-19 17:15:28 -0300

    Add pm2 ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps : [{
+    name: 'ajudaki-api',
+    script: './bin/www',
+    node_args: '-r esm -r dotenv/config.js',
+    instances: 1,
+    autorestart: true,
+    watch: true,
+    ignore_watch : ['node_modules', 'public'],
+    max_memory_restart: '512MB'
+  }]
+};
